d40e866680c5e7d03583f1f3f2c2a10e20981806
surendranvpvs <surendranvpvs@gmail.com>
2017-06-08 09:51:41 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,13 @@
+import java.until.*;
+public class oddeven
+{
+public static void main(String[]args)
+{
+int a;
+System.out.println("enter the number");
+if(a%2==0)
+system.out.println("the no is odd");
+else
+system.out.println("the no is even");
+}
+}
